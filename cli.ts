c01ad39e89a5989aee2cbc76e2bdafe68ac85b4b
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-06-23 19:02:51 -0300

    refactor: Handle CLI commands with a Fluent API

--- cli.ts
+++ cli.ts
@@ -1,55 +1,86 @@
 import { generateName, generateNameCustom } from "./mod.ts";
 
-const errorMessage =
-  "Wrong usage! If you need help use the h, -h or --help command";
-const supportedCommands = ["g", "-g", "--generate", "h", "-h", "--help"];
-const helpMessage =
-  `Supported commands are: [${supportedCommands.join(" ")}]\n` +
-  "For generation the syntax is {retry or separator} {retry?}";
+const genCommands = ["g", "-g", "--generate"];
+const helpMessage = `Supported commands are: [${genCommands.join(", ")}]
+    The syntax for generating is {retry? or separator?} {retry?} 
+    [? means optional parameter]
+    ------
+    The following are valid examples:
+    deno run .\\cli.ts
+    deno run .\\cli.ts g
+    deno run .\\cli.ts g "@"
+    deno run .\\cli.ts g 10
+    deno run .\\cli.ts g "@" 20
+    `;
+
+class CommandBuilder {
+  private baseCommand: string;
+  private retry?: number;
+  private separator?: string;
+
+  private constructor(base: string) {
+    this.baseCommand = base;
+  }
+
+  public static CreateCommand(base: string): CommandBuilder {
+    return new CommandBuilder(base);
+  }
+
+  public AddRetry(retry: number): CommandBuilder {
+    this.retry = retry;
+    return this;
+  }
+
+  public AddSeparator(separator: string): CommandBuilder {
+    this.separator = separator;
+    return this;
+  }
+
+  public BuildAndRun(): string {
+    // TODO: Further refactor this ugly bit of code
+    if (genCommands.includes(this.baseCommand)) {
+      if (this.retry && this.separator) {
+        return generateNameCustom(this.separator, this.retry);
+      } else if (this.separator) {
+        return generateNameCustom(this.separator);
+      } else if (this.retry) {
+        return generateName(this.retry);
+      } else {
+        return generateName();
+      }
+    }
+
+    return helpMessage;
+  }
+}
 
 /**
- * Handles command execution
- * @param command Command to be ran (check supported Commands)
- * @param firstArgs First argument, might be a separator or a retry number
- * @param secondArgs Second argument, should be a retry number or undefined
- */
+   * Handles command execution
+   * @param command Command to be ran (check supported Commands)
+   * @param firstArgs First argument, might be a separator or a retry number
+   * @param secondArgs Second argument, should be a retry number or undefined
+   */
 function handleCommand(
   command: string,
   firstArgs: string | number,
   secondArgs?: number,
 ) {
-  let result = "";
-  switch (command) {
-    case "g":
-    case "-g":
-    case "--generate":
-      if (typeof firstArgs === "number") {
-        result = generateName(firstArgs);
-      } else {
-        if (secondArgs && secondArgs != NaN) {
-          result = generateNameCustom(firstArgs, secondArgs);
-        } else {
-          result = generateNameCustom(firstArgs);
-        }
-      }
-      break;
-    case "h":
-    case "-h":
-    case "--help":
-    default:
-      result = helpMessage;
-      break;
+  const builder = CommandBuilder.CreateCommand(command);
+
+  typeof firstArgs === "number"
+    ? builder.AddRetry(firstArgs)
+    : builder.AddSeparator(firstArgs);
+
+  if (typeof secondArgs !== "undefined") {
+    builder.AddRetry(secondArgs);
   }
-  console.log(result);
+
+  return builder.BuildAndRun();
 }
 
-const [command, firstArgs, secondArgs] = Deno.args;
-if (supportedCommands.includes(command)) {
-  handleCommand(
-    command,
-    firstArgs,
-    secondArgs ? parseInt(secondArgs) : undefined,
-  );
-} else {
-  console.error(errorMessage);
-}
+const [command = "g", firstArgs, secondArgs] = Deno.args;
+console.log(handleCommand(
+  command,
+  firstArgs,
+  secondArgs ? parseInt(secondArgs) : undefined,
+));
