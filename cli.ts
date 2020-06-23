2c03d7b60a02b23f6a28a2380d41debb07c52b22
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-06-23 20:24:28 -0300

    refactor: Move from if-elses to a DecisionMatrix

--- cli.ts
+++ cli.ts
@@ -1,19 +1,53 @@
 import { generateName, generateNameCustom } from "./mod.ts";
 
 const genCommands = ["g", "-g", "--generate"];
-const helpMessage = `Supported commands are: [${genCommands.join(", ")}]
-    The syntax for generating is {retry? or separator?} {retry?} 
-    [? means optional parameter]
-    ------
-    The following are valid examples:
-    deno run .\\cli.ts
-    deno run .\\cli.ts g
-    deno run .\\cli.ts g "@"
-    deno run .\\cli.ts g 10
-    deno run .\\cli.ts g "@" 20
-    `;
+const helpMessage = `
+  Supported commands are: [${genCommands.join(", ")}]
+  The syntax for generating is {separator?} {retry?} 
+  [? means optional parameter]
+  ------
+  The following are valid examples:
+    deno run .\\cli.ts ... results in something_somegion
+    deno run .\\cli.ts g "@" ... results in something@someone
+    deno run .\\cli.ts g 10 ... results in something_someone10
+    deno run .\\cli.ts g 20 "@" ... results in something@someone20`;
 
+/**
+ * Describes a rule for running the name generation via CLI.
+ */
+interface GenerationStrategy {
+  retry: boolean;
+  separator: boolean;
+  strategy: (retryAmount?: number, separatorString?: string) => string;
+}
+
+/**
+ * Builder for Commands.
+ */
 class CommandBuilder {
+  private generationDecisionMatrix: GenerationStrategy[] = [
+    {
+      retry: true,
+      separator: true,
+      strategy: (retry, separator) =>
+        generateNameCustom(separator as string, retry),
+    },
+    {
+      retry: true,
+      separator: false,
+      strategy: (retry, _separator) => generateName(retry),
+    },
+    {
+      retry: false,
+      separator: true,
+      strategy: (_retry, separator) => generateNameCustom(separator as string),
+    },
+    {
+      retry: false,
+      separator: false,
+      strategy: (_retry, _separator) => generateName(),
+    },
+  ];
   private baseCommand: string;
   private retry?: number;
   private separator?: string;
@@ -27,6 +61,9 @@ class CommandBuilder {
   }
 
   public AddRetry(retry: number): CommandBuilder {
+    if (isNaN(retry)) {
+      return this;
+    }
     this.retry = retry;
     return this;
   }
@@ -37,17 +74,12 @@ class CommandBuilder {
   }
 
   public BuildAndRun(): string {
-    // TODO: Further refactor this ugly bit of code
     if (genCommands.includes(this.baseCommand)) {
-      if (this.retry && this.separator) {
-        return generateNameCustom(this.separator, this.retry);
-      } else if (this.separator) {
-        return generateNameCustom(this.separator);
-      } else if (this.retry) {
-        return generateName(this.retry);
-      } else {
-        return generateName();
-      }
+      return this.generationDecisionMatrix
+        .filter((p) =>
+          p.retry === (typeof this.retry !== "undefined") &&
+          p.separator === (typeof this.separator !== "undefined")
+        )[0]?.strategy(this.retry as number, this.separator as string);
     }
 
     return helpMessage;
@@ -62,25 +94,27 @@ class CommandBuilder {
    */
 function handleCommand(
   command: string,
-  firstArgs: string | number,
-  secondArgs?: number,
+  firstArgs?: string,
+  secondArgs?: string,
 ) {
   const builder = CommandBuilder.CreateCommand(command);
 
-  typeof firstArgs === "number"
-    ? builder.AddRetry(firstArgs)
-    : builder.AddSeparator(firstArgs);
+  if (typeof firstArgs !== "undefined") {
+    isNaN(parseInt(firstArgs))
+      ? builder.AddSeparator(firstArgs)
+      : builder.AddRetry(parseInt(firstArgs));
+  }
 
   if (typeof secondArgs !== "undefined") {
-    builder.AddRetry(secondArgs);
+    builder.AddSeparator(secondArgs);
   }
 
   return builder.BuildAndRun();
 }
 
-const [command = "g", firstArgs, secondArgs] = Deno.args;
+const [command = "g", retry, separator] = Deno.args;
 console.log(handleCommand(
   command,
-  firstArgs,
-  secondArgs ? parseInt(secondArgs) : undefined,
+  retry,
+  separator,
 ));
